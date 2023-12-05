b55cd560b87c140dfffb2771803b94433aa34898
jyotimishra05 <jyotihra28@gmail.com>
2023-12-05 17:48:29 +0530

    Created the signup page funtionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+  const [formData , setFormData]=useState({})
+  const handlechange=(e)=>{
+    console.log(e.target.value)
+    setFormData({
+      ...formData ,//indorder to have the track of previous data;
+      [e.target.id]:e.target.value
+      
+    })
+
+  }
+
+  const handleSubmit=async(e)=>{
+      e.preventDefault();
+      // const res= await fetch("/api/auth/signup",formData)
+      const res= await fetch("/api/auth/signup",{
+        method:"POST",
+       headers:{
+        'Content-Type':'application/json'
+       },
+       body:JSON.stringify(formData) //When sending data to a web server, the data has to be a string.
+       
+      })
+
+      const data = await res.json();
+      console.log(data);
+
+  }
   return (
   
    <div className='p-3 max-w-lg mx-auto'>
    <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-   <form className='flex flex-col gap-4'>
+   <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
    <input type='text'
     placeholder='username' 
     className="border p-3 rounded-lg"
     id='username'
+    onChange={handlechange}
    />
    <input type='email'
     placeholder='email' 
     className="border p-3 rounded-lg"
     id='email'
+    onChange={handlechange}
    />
    <input type='password'
     placeholder='password' 
     className="border p-3 rounded-lg"
     id='password'
+    onChange={handlechange}
    />
    <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
    </form>
