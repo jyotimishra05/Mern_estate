7bc7d8049c8ce4f26ffdedc1c534726e420591d1
jyotimishra05 <jyotihra28@gmail.com>
2023-12-08 00:23:28 +0530

    Added redux persist

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,14 +1,52 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer  from "./user/userSlice"
+// import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// import userReducer  from "./user/userSlice"
+// import persistReducer from 'redux-persist/es/persistReducer';
+// import storage from 'redux-persist/lib/storage';
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+// const rootReducer= combineReducers({user:userReducer}) ; //add reducers we have ex ; user
+
+// const persistConfig={
+//     key:'root',
+//     storage,
+//     version:1
+// }
+// const persistedReducer = persistReducer(persistConfig ,rootReducer);
+// export const store = configureStore({
+//   reducer: persistedReducer,
+// //   {
+// //     // user:userReducer
+
+// //   },
+//   //in order to check the serializable check other gives error later
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//     }),  //to prevent any error in your browser
+// })
+//  export const persistor = persistStore(store);
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user:userReducer
-  },
-  //in order to check the serializable check other gives error later
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),  //to prevent any error in your browser
-})
+    }),
+});
 
+export const persistor = persistStore(store);
